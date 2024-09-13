15657bffdd76a5c2ceccea10b225731423b63ff1
dom767 <dominicpenfold@gmail.com>
2024-09-13 19:31:23 +0100

    LAtest

---
+++ buildmandelbulb.cmd
@@ -0,0 +1,3 @@
+del index.php
+echo ^<?php /* Template Name: Mandelbulb Renderer */ ?^> > index.php
+type mandelbulb.html >> index.php
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php /* Template Name: Mandelbulb Renderer */ ?> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
