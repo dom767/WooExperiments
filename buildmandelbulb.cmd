69f27178496745bd2c7de4a71dbb92b1a1a107d5
dom767 <dominicpenfold@gmail.com>
2024-10-13 14:53:34 +0100

    Fractal updates
    Various fixes for fractal renderers including touch support etc.

--- buildmandelbulb.cmd
+++ buildmandelbulb.cmd
@@ -1,3 +1,19 @@
+mkdir Mandelbulb
+cd Mandelbulb
 del index.php
-echo ^<?php /* Template Name: Mandelbulb Renderer */ ?^> > index.php
-type mandelbulb.html >> index.php
+cd..
+echo ^<?php /* Template Name: Mandelbulb Renderer */ ?^> > mandelbulb/index.php
+type mandelbulb.html >> mandelbulb/index.php
+mkdir Mandelbrot
+cd Mandelbrot
+del index.php
+cd..
+echo ^<?php /* Template Name: Mandelbrot Renderer */ ?^> > Mandelbrot/index.php
+type Mandelbrot.html >> Mandelbrot/index.php
+mkdir Bifurcation
+cd Bifurcation
+del index.php
+cd..
+echo ^<?php /* Template Name: Bifurcation Renderer */ ?^> > Bifurcation/index.php
+type Bifurcation.html >> Bifurcation/index.php
+pause
